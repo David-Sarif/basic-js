9c050e2a24e58eef3acf4731271dac04e270311f
David-Sarif <DavidSarif@yandex.ru>
2020-03-20 03:49:54 +0300

    feat:counted cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,17 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+//  1st solution
+
+  return matrix.flat().filter(cat => cat==="^^").length
+
+// 2nd solution
+//   return matrix.flat().reduce(function(sum,cat){
+//     if (cat==="^^"){
+//         sum++;
+//     }
+//     return sum
+// }, 0);
+
+
+// 3rd solution
+// return matrix.flat().reduce((sum,cat) => (cat==='^^') ? ++sum : sum, 0)
 };
