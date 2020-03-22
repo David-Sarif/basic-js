28415e2819e0ce3080054fe637f69a8b2ead0bff
David-Sarif <DavidSarif@yandex.ru>
2020-03-22 21:31:28 +0300

    feat:solved what date task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,7 +1,4 @@
-module.exports = function transform( arr ) {
-    if (!Array.isArray(arr))
-    {
-    throw 'Not array';
-    }
+module.exports = function transform(/* arr */) {
+    throw 'Not implemented';
     // remove line with error and write your code here
 };
