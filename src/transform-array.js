d13af9ea9d704585af0587cd637fccab4aafd845
David-Sarif <DavidSarif@yandex.ru>
2020-03-22 21:50:17 +0300

    feat:transform-array partially solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,29 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
+module.exports = function transform( arr ) {
+    if (!Array.isArray(arr)){
+    throw 'Not array';
+    }
+    for (let i=0;i<arr.length;i++){
+        if (arr[i]==='--discard-next'){
+            arr.splice[i,2]
+        }
+        if (arr[i]==='--discard-prev')
+        {   
+            arr.splice[i-1,2]
+        }
+        if (arr[i]==='--double-next')
+        {
+            arr.splice[i,0,i+1]
+            arr.splice[i,1]
+        }
+        if (arr[i]==='--double-prev')
+        {
+            arr.splice[i-1,0,i-1]
+            arr.splice[i,1]
+        }
+
+
+        
+    }
+    return arr
     // remove line with error and write your code here
 };
